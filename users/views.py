b1b0dd9dd14fff4767e93d826e298132225c2297
Sanja Mandic <sanja1mandic@gmail.com>
2026-05-31 18:14:03 +0200

    fix seed api

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from users.models import User
 from users.serializers import ProfileSerializer
-from rest_framework.views import APIView, Response
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from django.core.management import call_command
 
 class ProfileDetailApiView(generics.RetrieveAPIView):
@@ -21,6 +22,11 @@ class PublicProfileListApiView(generics.ListAPIView):
     queryset = User.objects.all()
     
 class SeedDatabaseApiView(APIView):
+
+    permission_classes = [IsAdminUser]
+
     def post(self, request):
+
         call_command("seed")
+
         return Response({"detail": "Database seeded successfully."})
