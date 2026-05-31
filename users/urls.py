912d2ae8178975523965f0817a916bbe2ffb5f43
Sanja Mandic <sanja1mandic@gmail.com>
2026-05-31 18:53:17 +0200

    create admin

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from .views import ProfileDetailApiView, PublicProfileDetailApiView, PublicProfileListApiView, SeedDatabaseApiView
+from .views import (
+    ProfileDetailApiView,
+    PublicProfileDetailApiView,
+    PublicProfileListApiView,
+    SeedDatabaseApiView,
+    CreateAdminApiView,
+)
 
 urlpatterns = [
     path("profile/", ProfileDetailApiView.as_view()),
     path("users/", PublicProfileListApiView.as_view()),
     path("users/<int:pk>/", PublicProfileDetailApiView.as_view()),
     path("admin/seed/", SeedDatabaseApiView.as_view()),
+    path("admin/create-admin/", CreateAdminApiView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.core.management import call_command
 
+
 class ProfileDetailApiView(generics.RetrieveAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
@@ -13,14 +14,17 @@ class ProfileDetailApiView(generics.RetrieveAPIView):
     def get_object(self):
         return self.request.user
 
+
 class PublicProfileDetailApiView(generics.RetrieveAPIView):
     serializer_class = ProfileSerializer
     queryset = User.objects.all()
-    
+
+
 class PublicProfileListApiView(generics.ListAPIView):
     serializer_class = ProfileSerializer
     queryset = User.objects.all()
-    
+
+
 class SeedDatabaseApiView(APIView):
 
     permission_classes = [IsAdminUser]
@@ -30,3 +34,20 @@ class SeedDatabaseApiView(APIView):
         call_command("seed")
 
         return Response({"detail": "Database seeded successfully."})
+
+
+class CreateAdminApiView(APIView):
+
+    authentication_classes = []
+
+    permission_classes = []
+
+    def post(self, request):
+
+        if not User.objects.filter(username="admin").exists():
+
+            User.objects.create_superuser(
+                username="admin", email="admin@venturo.com", password="admin123456"
+            )
+
+        return Response({"detail": "Admin created"})
